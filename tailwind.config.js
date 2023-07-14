d10f40fb7bd33b288a8b70bcd9f7a714623b58ed
Bi Nguyen <binguyen@jessies-air.lan>
2023-07-14 17:00:21 -0400

    Add custom configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,45 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      gridTemplateColumns: {
+        24: 'repeat(24, minmax(0, 1fr))',
       },
+      gridColumnStart: {
+        13: '13',
+        14: '14',
+        15: '15',
+        16: '16',
+        17: '17',
+        18: '18',
+        19: '19',
+        20: '20',
+        21: '21',
+        22: '22',
+        23: '23',
+        24: '24',
+      },
+      gridColumnEnd: {
+        13: '13',
+        14: '14',
+        15: '15',
+        16: '16',
+        17: '17',
+        18: '18',
+        19: '19',
+        20: '20',
+        21: '21',
+        22: '22',
+        23: '23',
+        24: '24',
+      },
+      maxHeight: {
+        550: '34.375rem',
+      },
+    },
+    container: {
+      center: true,
     },
   },
   plugins: [],
-}
+  darkMode: 'class',
+};
