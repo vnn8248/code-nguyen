22ab717d95e38f6809c8487626934e8fe580d631
Bi Nguyen <binguyen@jessies-air.lan>
2023-07-14 17:01:36 -0400

    Create about page

---
+++ app/about/page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const metadata = {
+  title: 'About Bi Nguyen',
+};
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>AboutPage</h1>
+    </div>
+  );
+};
+
+export default AboutPage;
