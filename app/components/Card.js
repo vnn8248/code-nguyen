a7b14540d2c10402b147ef36890107d3a245706e
Bi Nguyen <binguyen@jessies-air.lan>
2023-07-18 08:31:56 -0400

    Connect cards and routes

--- app/components/Card.js
+++ app/components/Card.js
@@ -3,12 +3,54 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 const Card = (props) => {
+  let start;
+  let end;
+  let bg;
+  let w;
+  let h;
+
+  // First project in grid
+  if (props.position === 1) {
+    bg = 'bg-[#40e2a1]';
+    start = 1;
+    end = 11;
+    w = 810;
+    h = 422;
+  }
+
+  // Second
+  if (props.position === 2) {
+    bg = 'bg-[#ff6666]';
+    start = 12;
+    end = 24;
+    w = 1300;
+    h = 600;
+  }
+
+  // Third
+  if (props.position === 3) {
+    bg = 'bg-[#7cb2e8]';
+    start = 1;
+    end = 14;
+    w = 810;
+    h = 422;
+  }
+
+  // Fourth
+  if (props.position === 4) {
+    bg = 'bg-[#ff69f8]';
+    start = 15;
+    end = 24;
+    w = 1300;
+    h = 600;
+  }
+
   return (
     <div
-      className={`border-2 ${props.start} ${props.end} rounded-3xl ${props.bg} text-slate-900`}
+      className={`border-2 col-start-${start} col-end-${end} rounded-3xl ${bg} text-slate-900`}
     >
       <Link
-        href=""
+        href={`projects/${props.slug}`}
         className="max-h-550 rounded-lg flex w-full relative cursor-pointer flex-col no-underline"
       >
         <div className="card-wrap flex flex-col w-full h-full relative overflow-hidden rounded-3xl py-10 px-16">
@@ -23,8 +65,8 @@ const Card = (props) => {
               <figure className="relative">
                 <Image
                   src={props.imageSrc}
-                  width={props.imageW}
-                  height={props.imageH}
+                  width={w}
+                  height={h}
                   loading="lazy"
                   alt={props.imageAlt}
                   className="max-w-max rounded-xl relative z-20 opacity-100"
--- app/page.js
+++ app/page.js
@@ -1,19 +1,60 @@
 import React from 'react';
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import Link from 'next/link';
 import Card from './components/Card';
 
+const getProjects = () => {
+  const projectsDir = 'projects';
+
+  const files = fs.readdirSync(path.join(projectsDir));
+
+  const projects = files.map((filename) => {
+    const fileContent = fs.readFileSync(
+      path.join(projectsDir, filename),
+      'utf-8'
+    );
+
+    const { data: frontMatter } = matter(fileContent);
+
+    return {
+      meta: frontMatter,
+      slug: filename.replace('.mdx', ''),
+    };
+  });
+
+  return projects;
+};
+
 const HomePage = () => {
+  const projects = getProjects();
+  console.log(projects);
+
   return (
     <div className="content">
-      <div className="text-7xl text-center pt-32 pb-12 lg:pt-48 lg:pb-12 font-bold tracking-tighter leading-tight">
-        <h1>Bi Nguyen.</h1>
-        <h1>Software Engineer.</h1>
+      <div className="text-7xl text-center pt-32 pb-12 lg:pt-48 lg:pb-12 font-bold tracking-tightest leading-tight">
+        <h1 className="">Hi. I'm Bi Nguyễn.</h1>
+        <h1 className="">A Software Engineer.</h1>
       </div>
       <div className="text-center px-12">
         <p>I'm passionate about creating things for others to enjoy.</p>
       </div>
       <div className="cards my-24 px-24">
         <div className="cards-content grid grid-cols-24 gap-y-16 max-w-screen-2xl mx-auto w-full">
-          <Card
+          {projects.map((project) => {
+            return (
+              <Card
+                projectName={project.meta.name}
+                projectCompany={project.meta.company}
+                imageSrc={project.meta.preview.image}
+                imageAlt={project.meta.preview.imageAlt}
+                position={project.meta.position}
+                slug={project.slug}
+              />
+            );
+          })}
+          {/* <Card
             bg={'bg-[#40e2a1]'}
             projectName={'3.16'}
             projectCompany={'K4'}
@@ -44,7 +85,7 @@ const HomePage = () => {
             imageH={600}
             imageAlt={'Cloud Mapping'}
             start={'col-start-1'}
-            end={'col-end-16'}
+            end={'col-end-14'}
           />
           <Card
             bg={'bg-[#ff69f8]'}
@@ -54,9 +95,9 @@ const HomePage = () => {
             imageW={810}
             imageH={422}
             imageAlt={'Thingsboard migration'}
-            start={'col-start-17'}
+            start={'col-start-15'}
             end={'col-end-24'}
-          />
+          /> */}
         </div>
       </div>
     </div>
