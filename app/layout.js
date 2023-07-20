8dbe98e3101618f12687f4570b600b48647a0a58
Bi Nguyen <binguyen@jessies-air.lan>
2023-07-20 17:22:05 -0400

    Add scroll effect

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,45 @@
+'use client';
+
 import './globals.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { useCallback, useEffect, useState } from 'react';
 
-export const metadata = {
-  title: 'Bi Nguyen',
-  description: "Bi Nguyen's online portfolio",
-  keywords:
-    'web development, javascript, next, vercel, react, tailwind, mdx, developer',
-};
+// export const metadata = {
+//   title: 'Bi Nguyen',
+//   description: "Bi Nguyen's online portfolio",
+//   keywords:
+//     'web development, javascript, next, vercel, react, tailwind, mdx, developer',
+// };
 
 export default function RootLayout({ children }) {
+  let scrolled = false;
+  const [scrollY, setScrollY] = useState(0);
+
+  const onScroll = useCallback((event) => {
+    const { scrollY } = window;
+
+    setScrollY(scrollY);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll, { passive: true });
+    // remove event on unmount to prevent a memory leak with the cleanup
+    return () => {
+      window.removeEventListener('scroll', onScroll, { passive: true });
+    };
+  }, []);
+
+  if (scrollY > 0) {
+    scrolled = true;
+  } else {
+    scrolled = false;
+  }
+
   return (
     <html lang="en">
       <body>
-        <Header />
+        <Header scrolled={scrolled} />
         <main>{children}</main>
         <Footer />
       </body>
