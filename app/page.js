a0b1e2955fd215f040051abbd8613869351c5941
Bi Nguyen <binguyen@bis-macbook-pro.lan>
2023-07-25 10:31:10 -0400

    Update styling

--- app/page.js
+++ app/page.js
@@ -32,18 +32,16 @@ const HomePage = () => {
 
   return (
     <div className="content relative w-full h-full z-10">
-      <h1 className="pt-32 pb-12 pl-8 pr-0 md:px-16 md:pt-32 lg:px-32 flex justify-left lg:text-7xl">
-        <span className=" ">
-          <span className="flex relative z-10 tracking-tightest">
+      <div className="pt-32 pb-12 pl-8 pr-0 flex flex-col justify-left md:px-16 lg:pt-48 lg:px-32">
+          <h1 className=" relative z-10 tracking-tightest">
             Hi. I'm Bi Nguyễn.
-          </span>
-          <span className="flex font-normal relative z-10">Developer,</span>
-          <span className="flex font-normal relative z-10">Writer,</span>
-          <span className="flex font-normal relative z-10">
+          </h1>
+          <h2 className=" font-normal relative z-10">Developer,</h2>
+          <h2 className=" font-normal relative z-10">Writer,</h2>
+          <h2 className=" font-normal relative z-10">
             & Life-long Learner.
-          </span>
-        </span>
-      </h1>
+          </h2>
+      </div>
       <div className="px-8 md:px-16 lg:px-32 lg:max-w-4xl relative">
         <p className="md:text-lg">
           I love writing. Whether it's coding in Javascript or writing on{' '}
@@ -55,7 +53,7 @@ const HomePage = () => {
           </Link>
           , the craft of communication is something that I am passionate about.
         </p>
-        <p className="mt-8 md:text-lg">
+        <p className="mt-6 md:text-lg">
           Right now, I am a Software Engineer II at{' '}
           <Link
             href="https://www.k4connect.com/"
@@ -65,11 +63,11 @@ const HomePage = () => {
           </Link>{' '}
           focusing on creating human-centric experiences through IoT devices.
         </p>
-        <p className="mt-8 md:text-lg">
+        <p className="mt-6 md:text-lg">
           Here are some projects that I'm proudest of:
         </p>
       </div>
-      <div className="cards my-12 md:my-24 px-8">
+      <div className="cards my-12 md:my-24 2xl:my-12 px-8">
         <div className="cards-content grid grid-cols-24 gap-y-24 max-w-screen-2xl mx-auto w-full">
           {projects.map((project) => {
             return (
@@ -80,6 +78,7 @@ const HomePage = () => {
                 imageAlt={project.meta.preview.imageAlt}
                 position={project.meta.position}
                 slug={project.slug}
+                key={project.meta.preview.imageAlt}
               />
             );
           })}
