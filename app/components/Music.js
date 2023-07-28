c05c5af3be4589b5a5a3b1ec7e3ddf6ef6f912e4
Bi Nguyen <binguyen@bis-macbook-pro.lan>
2023-07-28 17:17:54 -0400

    Add more music

--- app/components/Music.js
+++ app/components/Music.js
@@ -1,6 +1,6 @@
-'use client'
+'use client';
 
-import React from 'react'
+import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
@@ -25,7 +25,7 @@ const Music = () => {
         },
         {
             artist: 'Niels-Henning Ørsted Pedersen',
-            song: "Morning Fun",
+            song: 'Morning Fun',
             url: 'https://open.spotify.com/track/6erpkauC5PeFkRKO6Oj7rL?si=671b5cf7d44840a8'
         },
         {
@@ -35,13 +35,33 @@ const Music = () => {
         },
         {
             artist: 'Rodrigo y Gabriela',
-            song: "Ixtapa",
+            song: 'Ixtapa',
             url: 'https://open.spotify.com/track/3augrYFaThwiMRYjDScWmv?si=1208c7de679542e3'
         },
         {
             artist: 'Martin Hayes Quartet',
             song: "Brennan's Reel",
             url: 'https://open.spotify.com/track/3HidYV1TitmxDpP0gTNPti?si=3bc5f63b42104142'
+        },
+        {
+            artist: 'Thelonious Monk',
+            song: 'North of The Sunset',
+            url: 'https://open.spotify.com/track/0uy4Zg0yVfzMyqRG9bkRnK?si=80627ae09f4b4504'
+        },
+        {
+            artist: 'Little Brother',
+            song: 'The Way You Do It',
+            url: 'https://open.spotify.com/track/2K1egclbZdZMjtxXswQznk?si=6a258cc64dbf4a3e'
+        },
+        {
+            artist: 'Pete Rock, InI',
+            song: 'Microphonist Wanderlust',
+            url: 'https://open.spotify.com/track/3rZd9MtjF6Wp9jzcXOCf7L?si=0a027f0b095548e8'
+        },
+        {
+            artist: 'Duke Ellington, John Coltrane',
+            song: 'Stevie',
+            url: 'https://open.spotify.com/track/6Ylpg33fqhQfJrXLwSxnzh?si=f3cf885ec4704257'
         }
     ];
 
@@ -53,28 +73,19 @@ const Music = () => {
         <div className="fun col-start-3 col-end-23 md:col-end-15 xl:col-start-6 xl:col-end-13">
             <div className="faded flex items-center rounded-full overflow-hidden py-4 pl-5 pr-10 transition-all duration-200 ease-in-out">
                 <div className="icon flex rounded-full opacity-100">
-                    <Image
-                    src={'/icons/spotify.svg'}
-                    width={40}
-                    height={40}
-                    alt={'Spotify'}
-                    />
+                    <Image src={'/icons/spotify.svg'} width={40} height={40} alt={'Spotify'} />
                 </div>
                 <div className="track-list flex flex-col overflow-hidden flex-1 ml-5">
                     <div className="artist opacity-80 text-sm">Now Playing...</div>
                     <div className="song font-bold tracking-normal overflow-ellipsis overflow-hidden whitespace-nowrap text-sm">
-                    <Link
-                        href={song.url}
-                        target="_blank"
-                        rel="nofollow noreferrer"
-                    >
-                        {`${song.song} by ${song.artist}`}
-                    </Link>
+                        <Link href={song.url} target="_blank" rel="nofollow noreferrer">
+                            {`${song.song} by ${song.artist}`}
+                        </Link>
                     </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Music;
