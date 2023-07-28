f460cb2cafae252a71e192f64f2b38cb3d188a6f
Bi Nguyen <binguyen@bis-macbook-pro.lan>
2023-07-28 17:15:25 -0400

    Create personal project component

---
+++ app/components/PersonalProject.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+const PersonalProject = (props) => {
+    return props.comingSoon ? (
+        <div className="col-span-4 md:col-span-2 rounded-xl faded cursor-not-allowed no-underline overflow-hidden relative transition-all ease-linear duration-300">
+            <h4 className="pl-4 bg-rose-400">Coming soon!</h4>
+            <div className="showcase-inner flex justify-start items-start relative h-full gap-3.5 p-6 md:px-10 md:pb-10 md:pt-2">
+                <div className="details flex flex-1 flex-col gap-4">
+                    <div className="title font-bold flex relative">
+                        <h4>{props.title}</h4>
+                    </div>
+                    <p className="description meta">{props.description}</p>
+                </div>
+                <div className="shot-holder h-full flex items-center">
+                    <div className="overflow-hidden rounded">
+                        <figure className="relative">
+                            <Image src={props.src} width={100} height={100} className="" />
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </div>
+    ) : (
+        <Link
+            href={props.url}
+            className="col-span-4 md:col-span-2 rounded-xl faded cursor-pointer no-underline overflow-hidden relative transition-all ease-linear duration-300"
+            target="__blank"
+        >
+            <div className="showcase-inner flex justify-start items-start relative h-full gap-3.5 p-6 md:p-10">
+                <div className="details flex flex-1 flex-col gap-4">
+                    <div className="title font-bold flex relative">
+                        <h4>{props.title}</h4>
+                    </div>
+                    <p className="description meta">{props.description}</p>
+                </div>
+                <div className="shot-holder h-full flex items-center">
+                    <div className="overflow-hidden rounded">
+                        <figure className="relative">
+                            <Image src={props.src} width={100} height={100} className="" />
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </Link>
+    );
+};
+
+export default PersonalProject;
--- app/personal/page.js
+++ app/personal/page.js
@@ -1,166 +1,60 @@
 import React from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
+import PersonalProject from '../components/PersonalProject';
 
 export const metadata = {
-  title: 'Personal Projects',
+    title: 'Personal Projects'
 };
 
 const PersonalPage = () => {
-  return (
-    <div className="content">
-      <div className="text-7xl text-center pt-32 pb-12 md:pt-48 lg:pb-12 font-bold tracking-tightest leading-tight">
-        <h1 className="">Personal projects.</h1>
-      </div>
-      <div className="text-center px-12">
-        <p>I'm passionate about creating things for others to enjoy.</p>
-      </div>
-      <div className="showcase-wrapper px-10 md:p-10">
-        <div className="showcase grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 max-w-screen-2xl mx-auto my-20 gap-10">
-          <Link
-            href={''}
-            className="col-span-4 md:col-span-2 rounded-xl faded cursor-pointer no-underline overflow-hidden relative transition-all ease-linear duration-300"
-          >
-            <div className="showcase-inner flex justify-start items-start relative h-full gap-3.5 p-6 md:p-10">
-              <div className="details flex flex-1 flex-col gap-4">
-                <div className="title font-bold flex relative">
-                  <h4>Wire Box</h4>
-                </div>
-                <p className="description meta">
-                  Turn hi-fi designs to lo-fi in one click - comes in nasty
-                  pink.
-                </p>
-              </div>
-              <div className="shot-holder h-full flex items-center">
-                <div className="overflow-hidden rounded">
-                  <figure className="relative">
-                    <Image
-                      src={'/icons/spotify.svg'}
-                      width={100}
-                      height={100}
-                      className=""
-                    />
-                  </figure>
-                </div>
-              </div>
+    return (
+        <div className="content">
+            <div className="text-7xl text-center pt-32 pb-12 md:pt-48 lg:pb-12 font-bold tracking-tightest leading-tight">
+                <h1 className="">Personal projects.</h1>
             </div>
-          </Link>
-          <Link
-            href={''}
-            className="col-span-4 md:col-span-2 rounded-xl faded cursor-pointer no-underline overflow-hidden relative transition-all ease-linear duration-300"
-          >
-            <div className="showcase-inner flex justify-start items-start relative h-full gap-3.5 p-6 md:p-10">
-              <div className="details flex flex-1 flex-col gap-4">
-                <div className="title font-bold flex relative">
-                  <h4>Wire Box</h4>
-                </div>
-                <p className="description meta">
-                  Turn hi-fi designs to lo-fi in one click - comes in nasty
-                  pink.
-                </p>
-              </div>
-              <div className="shot-holder h-full flex items-center">
-                <div className="overflow-hidden rounded">
-                  <figure className="relative">
-                    <Image
-                      src={'/icons/avataaars.svg'}
-                      width={100}
-                      height={100}
-                      className=""
-                    />
-                  </figure>
-                </div>
-              </div>
+            <div className="text-center px-12">
+                <p>I'm passionate about creating things for others to enjoy.</p>
             </div>
-          </Link>
-          <Link
-            href={''}
-            className="col-span-4 md:col-span-2 rounded-xl faded cursor-pointer no-underline overflow-hidden relative transition-all ease-linear duration-300"
-          >
-            <div className="showcase-inner flex justify-start items-start relative h-full gap-3.5 p-6 md:p-10">
-              <div className="details flex flex-1 flex-col gap-4">
-                <div className="title font-bold flex relative">
-                  <h4>Wire Box</h4>
-                </div>
-                <p className="description meta">
-                  Turn hi-fi designs to lo-fi in one click - comes in nasty
-                  pink.
-                </p>
-              </div>
-              <div className="shot-holder h-full flex items-center">
-                <div className="overflow-hidden rounded">
-                  <figure className="relative">
-                    <Image
-                      src={'/icons/avataaars.svg'}
-                      width={100}
-                      height={100}
-                      className=""
+            <div className="showcase-wrapper px-10 md:p-10">
+                <div className="showcase grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 max-w-screen-2xl mx-auto my-20 gap-10">
+                    <PersonalProject
+                        title="Code Nguyen"
+                        src="/icons/avataaars.svg"
+                        description="My first portfolio!"
+                        url="https://www.codenguyen.com/"
+                        comingSoon={false}
                     />
-                  </figure>
-                </div>
-              </div>
-            </div>
-          </Link>
-          <Link
-            href={''}
-            className="col-span-4 md:col-span-2 rounded-xl faded cursor-pointer no-underline overflow-hidden relative transition-all ease-linear duration-300"
-          >
-            <div className="showcase-inner flex justify-start items-start relative h-full gap-3.5 p-6 md:p-10">
-              <div className="details flex flex-1 flex-col gap-4">
-                <div className="title font-bold flex relative">
-                  <h4>Wire Box</h4>
-                </div>
-                <p className="description meta">
-                  Turn hi-fi designs to lo-fi in one click - comes in nasty
-                  pink.
-                </p>
-              </div>
-              <div className="shot-holder h-full flex items-center">
-                <div className="overflow-hidden rounded">
-                  <figure className="relative">
-                    <Image
-                      src={'/icons/avataaars.svg'}
-                      width={100}
-                      height={100}
-                      className=""
+                    <PersonalProject
+                        title="We Are The Schnguyenies"
+                        src="/projects/whitesands.png"
+                        description="A custom website for my wife and I."
+                        url="https://www.wearetheschnguyenies.com/"
+                        comingSoon={false}
                     />
-                  </figure>
-                </div>
-              </div>
-            </div>
-          </Link>
-          <Link
-            href={''}
-            className="col-span-4 md:col-span-2 rounded-xl faded cursor-pointer no-underline overflow-hidden relative transition-all ease-linear duration-300"
-          >
-            <div className="showcase-inner flex justify-start items-start relative h-full gap-3.5 p-6 md:p-10">
-              <div className="details flex flex-1 flex-col gap-4">
-                <div className="title font-bold flex relative">
-                  <h4>Wire Box</h4>
-                </div>
-                <p className="description meta">
-                  Turn hi-fi designs to lo-fi in one click - comes in nasty
-                  pink.
-                </p>
-              </div>
-              <div className="shot-holder h-full flex items-center">
-                <div className="overflow-hidden rounded">
-                  <figure className="relative">
-                    <Image
-                      src={'/icons/avataaars.svg'}
-                      width={100}
-                      height={100}
-                      className=""
+                    <PersonalProject
+                        title="Pickup At The Park"
+                        src="/icons/boots.svg"
+                        description="My Substack about The Beautiful Game."
+                        url="https://binguyen.substack.com/"
+                        comingSoon={false}
                     />
-                  </figure>
+                    <PersonalProject
+                        title="Fear & Gluttony"
+                        src="/projects/gluttony.jpeg"
+                        description="My Substack about my food adventures."
+                        url="https://fearandgluttony.substack.com/"
+                        comingSoon={false}
+                    />
+                    <PersonalProject
+                        title="Hip Hop Time Travelers"
+                        src="/icons/backpack.svg"
+                        description="My Substack about the greatest rabbit hole I've ever fell down - 90's Hip Hop."
+                        url="https://fearandgluttony.substack.com/"
+                        comingSoon={true}
+                    />
                 </div>
-              </div>
             </div>
-          </Link>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default PersonalPage;
