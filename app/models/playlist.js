0d16afc0a7c54a319e2b13364eb0914677a5c648
Bi Nguyen <binguyen@jessies-air.lan>
2023-08-03 07:16:32 -0400

    Add more music

---
+++ app/models/playlist.js
@@ -0,0 +1,59 @@
+const playlist = [
+  {
+    artist: 'De La Soul',
+    song: 'Betta Listen',
+    url: 'https://open.spotify.com/track/2db8VSxfFglwMPmzjbYMS0?si=122e41f795714b1f',
+  },
+  {
+    artist: 'SpinalE',
+    song: "L'esperimento del Cocktail",
+    url: 'https://open.spotify.com/track/65WPqfj6BOqZgWjmPDdVCJ?si=ee9ce7b8732740d9',
+  },
+  {
+    artist: 'Niels-Henning Ørsted Pedersen',
+    song: 'Morning Fun',
+    url: 'https://open.spotify.com/track/6erpkauC5PeFkRKO6Oj7rL?si=671b5cf7d44840a8',
+  },
+  {
+    artist: 'Jazz Liberatorz',
+    song: "Cin D'oeil",
+    url: 'https://open.spotify.com/track/0Smxi47vM9rcOiUXsYuxER?si=fcaa7c6a447049de',
+  },
+  {
+    artist: 'Rodrigo y Gabriela',
+    song: 'Ixtapa',
+    url: 'https://open.spotify.com/track/3augrYFaThwiMRYjDScWmv?si=1208c7de679542e3',
+  },
+  {
+    artist: 'Martin Hayes Quartet',
+    song: "Brennan's Reel",
+    url: 'https://open.spotify.com/track/3HidYV1TitmxDpP0gTNPti?si=3bc5f63b42104142',
+  },
+  {
+    artist: 'Thelonious Monk',
+    song: 'North of The Sunset',
+    url: 'https://open.spotify.com/track/0uy4Zg0yVfzMyqRG9bkRnK?si=80627ae09f4b4504',
+  },
+  {
+    artist: 'Little Brother',
+    song: 'The Way You Do It',
+    url: 'https://open.spotify.com/track/2K1egclbZdZMjtxXswQznk?si=6a258cc64dbf4a3e',
+  },
+  {
+    artist: 'Pete Rock, InI',
+    song: 'Microphonist Wanderlust',
+    url: 'https://open.spotify.com/track/3rZd9MtjF6Wp9jzcXOCf7L?si=0a027f0b095548e8',
+  },
+  {
+    artist: 'Duke Ellington, John Coltrane',
+    song: 'Stevie',
+    url: 'https://open.spotify.com/track/6Ylpg33fqhQfJrXLwSxnzh?si=f3cf885ec4704257',
+  },
+  {
+    artist: 'Reflection Eternal',
+    song: 'Love Speakeasy',
+    url: 'https://open.spotify.com/track/6eXpxFGhRMssC5XnaxEhMu?si=b6c2c59df4f24820',
+  },
+];
+
+export default playlist;
