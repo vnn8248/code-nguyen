4c36a3f0f4d00bf30e33c3362cb34a105929f009
Bi Nguyen <binguyen@bis-macbook-pro.lan>
2023-08-02 21:24:27 -0400

    Add styling for ul

--- app/projects/[slug]/page.js
+++ app/projects/[slug]/page.js
@@ -46,7 +46,7 @@ export default function Post({ params }) {
     const props = getPost(params);
 
     return (
-        <article className="prose max-w-screen-xl mx-auto prose-h1:text-center prose-h4:m-1 px-6 pt-32 pb-12 mb-0 sm:prose-h2:ml-16 sm:prose-p:mx-16 sm:prose-h3:text-center sm:prose-h3:mt-24 md:pt-48 lg:pb-16 lg:prose-p:mx-32 lg:prose-h2:ml-32 lg:prose-h3:text-2xl">
+        <article className="prose max-w-screen-xl mx-auto prose-h1:text-center prose-h4:m-1 px-6 prose-ul:px-6 pt-32 pb-12 mb-0 sm:prose-h2:ml-16 sm:prose-p:mx-16 sm:prose-h3:text-center sm:prose-h3:mt-24 md:pt-48 md:prose-ul:pl-24 lg:pb-16 lg:prose-ul:pl-44 lg:prose-p:mx-32 lg:prose-h2:ml-32 lg:prose-h3:text-2xl">
             <h1 className="text-5xl text-center font-bold tracking-tighter leading-tight sm:text-7xl">
                 {props.frontMatter.title}
             </h1>
